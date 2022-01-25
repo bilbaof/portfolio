66dd52d84f2f395c9a4e4d0fc4a1fa0005a0b1e5
bilbaof <fran_bilbao89@hotmail.com>
2022-01-25 22:09:58 +0100

    cambios de php

--- proyectos.php
+++ proyectos.php
@@ -1,3 +1,9 @@
+<?php
+
+$pg = "proyectos";
+
+?>
+
 <!DOCTYPE html>
 <html lang="es">
 
@@ -15,36 +21,7 @@
 </head>
 
 <body id="proyectos">
-    <header class="container px-0">
-        <nav class="navbar navbar-expand-md px-0">
-            <div class="container-fluid">
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse"
-                    aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-                    <i class="fas fa-bars"></i>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarCollapse">
-                    <ul class="navbar-nav me-auto mb-2 mb-md-0 mt-2">
-                        <li class="nav-item">
-                            <a class="nav-link px-4 py-1" href="index.php">Inicio</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link px-4 py-1" href="sobre-mi.php">Sobre mi</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link px-4 py-1 active" href="proyectos.php">Proyectos</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link py-1 px-4 " href="contacto.php">Contacto</a>
-                        </li>
-                    </ul>
-                    <div class="d-inline">
-                        <a href="" class="btn btn-rojo">Descargar mi CV <i class="fas fa-download"></i></a>
-                    </div>
-                </div>
-
-            </div>
-        </nav>
-    </header>
+    <?php include_once "header.php";?>
     <main class="container">
         <div class="row">
             <div class="col-12 py-4 pb-5 mt-2">
--- sobre-mi.php
+++ sobre-mi.php
@@ -1,3 +1,9 @@
+<?php
+
+$pg = "sobre-mi";
+
+?>
+
 <!DOCTYPE html>
 <html lang="es">
 
@@ -15,36 +21,7 @@
 </head>
 
 <body id="sobre-mi">
-    <header class="container px-0">
-        <nav class="navbar navbar-expand-md px-0">
-            <div class="container-fluid">
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse"
-                    aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-                    <i class="fas fa-bars"></i>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarCollapse">
-                    <ul class="navbar-nav me-auto mb-2 mb-md-0 mt-2">
-                        <li class="nav-item">
-                            <a class="nav-link px-4 py-1" href="index.php">Inicio</a>
-                        </li>
-                        <li class="nav-item ">
-                            <a class="nav-link px-4 py-1 active" href="sobre-mi.php">Sobre mí</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link px-4 py-1" href="proyectos.php">Proyectos</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link px-4 py-1" href="contacto.php">Contacto</a>
-                        </li>
-                    </ul>
-                    <div class="d-inline">
-                        <a href="" class="btn btn-rojo">Descargar mi CV <i class="fas fa-download"></i></a>
-                    </div>
-                </div>
-
-            </div>
-        </nav>
-    </header>
+    <?php include_once "header.php";?>  
     <main>
         <div class="container">
             <div class="row">
